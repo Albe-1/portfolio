a7c8e93c72066ff2e6286a31d861d569176d6ba9
Alberto <albegi.ag@gmail.com>
2025-11-16 14:53:52 +0100

    Update Chiave_1.py

--- Chiave_1.py
+++ Chiave_1.py
@@ -6,17 +6,17 @@ import sys
 messagge = input("Write a message: ")
 
 while True:
-    chiave_input = input("Write the key: ")
-    if len(chiave_input) < 10:
+    key_input = input("Write the key: ")
+    if len(key_input) < 10:
         break
     print("Error: the key must be less than 10 characters.", file=sys.stderr)
 
-hash_hex = hashlib.sha256(chiave_input.encode('utf-8')).hexdigest()
+hash_hex = hashlib.sha256(key_input.encode('utf-8')).hexdigest()
 
-#print("Key:", chiave_input)
+#print("Key:", key_input)
 #print("SHA256:", hash_hex)
 
-file_chiave = open("key.txt", "w")
-file_chiave.write(hash_hex + "\n")
-file_chiave.write(messagge)
-file_chiave.close()
+file_key = open("key.txt", "w")
+file_key.write(hash_hex + "\n")
+file_key.write(messagge)
+file_key.close()
