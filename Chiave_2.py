eb249acb97deeb2eed07849b5a9b47eb34780b32
Alberto <albegi.ag@gmail.com>
2025-11-16 14:51:49 +0100

    Update Chiave_2.py

--- Chiave_2.py
+++ Chiave_2.py
@@ -1,22 +1,21 @@
 #Alberto Gibellato 5BI
-#Leggo da file la chiave criptata e la confronto con quella inserita
 
 import hashlib
 import sys
 
-chiave_input2 = input("Scrivere la chiave per visualizzare il messaggio: ")
+key_input2 = input("Write the key to view the message: ")
 
-hash_hex2 = hashlib.sha256(chiave_input2.encode('utf-8')).hexdigest()
+hash_hex2 = hashlib.sha256(key_input2.encode('utf-8')).hexdigest()
 
-lettura_file = open("chiave.txt", "r")
-riga_chiave = lettura_file.readline().strip()
-riga_messaggio = lettura_file.read()
+reading_file = open("key.txt", "r")
+line_key = reading_file.readline().strip()
+line_messagge = reading_file.read()
 
-#print("chiave: " + riga_chiave)
-lettura_file.close()
+#print("key: " + line_key)
+reading_file.close()
 
-if hash_hex2 == riga_chiave:
-    print("Chiave accettata")
-    print("Messaggio: ", riga_messaggio)
+if hash_hex2 == line_key:
+    print("Key accepted")
+    print("Messagge: ", line_messagge)
 else:
-    print("Chiave non accettata")
+    print("Key not accepted")
